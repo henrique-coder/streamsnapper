a58566bf56f913ab4b570c23dd7c4feb6d569a11
Henrique <henriquemoreira10fk@gmail.com>
2024-10-30 16:42:38 -0300

    Enchanced exception docstrings

--- streamsnapper/exceptions.py
+++ streamsnapper/exceptions.py
@@ -1,30 +1,28 @@
 class StreamBaseError(Exception):
-    """
-    Base exception for StreamSnapper errors.
-    """
+    """Base exception for StreamSnapper errors."""
 
     pass
 
 
 class InvalidDataError(StreamBaseError):
-    """
-    Exception raised when invalid yt-dlp data is provided.
-    """
+    """Exception raised when invalid yt-dlp data is provided."""
 
     pass
 
 
 class ScrapingError(StreamBaseError):
-    """
-    Exception raised when an error occurs while scraping YouTube data.
-    """
+    """Exception raised when an error occurs while scraping YouTube data."""
 
     pass
 
 
 class DownloadError(StreamBaseError):
-    """
-    Exception raised when an error occurs while downloading a file.
-    """
+    """Exception raised when an error occurs while downloading a file."""
+
+    pass
+
+
+class MergeError(StreamBaseError):
+    """Exception raised when an error occurs while merging files."""
 
     pass
