938f834e83878d7fa189da003971f8732297a734
Henrique <henriquemoreira10fk@gmail.com>
2024-10-30 16:43:10 -0300

    Added automatic path converter function

--- streamsnapper/functions.py
+++ streamsnapper/functions.py
@@ -1,7 +1,9 @@
 # Built-in imports
+from pathlib import Path
+from os import PathLike
 from re import sub as re_sub
 from unicodedata import normalize
-from typing import Any, Dict, Optional, Callable
+from typing import Any, Union, Dict, Optional, Callable
 
 
 def get_value(data: Dict[Any, Any], key: Any, fallback_key: Any = None, convert_to: Callable = None, default_to: Optional[Any] = None) -> Any:
@@ -56,3 +58,30 @@ def format_string(query: str, max_length: int = 128) -> Optional[str]:
         sanitized_string = sanitized_string[:cutoff] if cutoff != -1 else sanitized_string[:max_length]
 
     return sanitized_string if sanitized_string else None
+
+
+def convert_to_path(file_path: Union[str, PathLike], check_if_exists: bool = True, check_if_is_file: bool = False, check_if_is_dir: bool = False) -> Path:
+    """
+    Convert a file path to a Path object. If the file does not exis, raise a FileExistsError. If the file is not a file, raise a FileNotFoundError. If the file is not a directory, raise a FileNotFoundError.
+
+    :param file_path: The file path to convert.
+    :param check_if_exists: If True, raise a FileExistsError if the file does not exist.
+    :param check_if_is_file: If True, raise a FileNotFoundError if the file is not a file.
+    :param check_if_is_dir: If True, raise a FileNotFoundError if the file is not a directory.
+    :raises FileExistsError: If the file does not exist and check_if_exists is True.
+    :raises FileNotFoundError: If the file is not a file and check_if_is_file is True or the file is not a directory and check_if_is_dir is True.
+    :return: The Path object.
+    """
+
+    file_path = Path(file_path)
+
+    if check_if_exists and not file_path.exists():
+        raise FileExistsError(f'The path "{file_path}" does not exist.')
+
+    if check_if_is_file and not file_path.is_file():
+        raise FileNotFoundError(f'The path "{file_path}" is not a file.')
+
+    if check_if_is_dir and not file_path.is_dir():
+        raise FileNotFoundError(f'The path "{file_path}" is not a directory.')
+
+    return file_path
