77ee3044dfd4c00d6a957213e254f8a026a6c5f6
Henrique <henriquemoreira10fk@gmail.com>
2024-10-30 14:46:37 -0300

    Optimized __init__ imports

--- streamsnapper/__init__.py
+++ streamsnapper/__init__.py
@@ -1,6 +1,6 @@
 # Local imports
 from .streamsnapper import YouTube, SoundCloud, Downloader
-from .exceptions import *
+from .exceptions import StreamBaseError, InvalidDataError, ScrapingError, DownloadError
 
 
 __version__ = '0.0.8'
