cf5dc8522603227ec31fc9219a4c97fd7102ff5b
Henrique <henriquemoreira10fk@gmail.com>
2024-10-31 10:30:49 -0300

    Bump version to 0.0.9

--- streamsnapper/__init__.py
+++ streamsnapper/__init__.py
@@ -3,5 +3,5 @@ from .streamsnapper import YouTube, SoundCloud, Downloader, Merger
 from .exceptions import StreamBaseError, InvalidDataError, ScrapingError, DownloadError, MergeError
 
 
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 __license__ = 'MIT'
