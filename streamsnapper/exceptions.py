8c9a14b1a958f04aa73f55a5f8008845b15c20fa
Henrique <henriquemoreira10fk@gmail.com>
2024-11-08 13:42:18 -0300

    Formatted exceptions file

--- streamsnapper/exceptions.py
+++ streamsnapper/exceptions.py
@@ -3,25 +3,30 @@ class StreamBaseError(Exception):
 
     pass
 
+
 class EmptyDataError(StreamBaseError):
     """Exception raised when no data is available."""
 
     pass
+
 
 class InvalidDataError(StreamBaseError):
     """Exception raised when invalid data is provided."""
 
     pass
 
+
 class ScrapingError(StreamBaseError):
     """Exception raised when an error occurs while scraping data."""
 
     pass
 
+
 class DownloadError(StreamBaseError):
     """Exception raised when an error occurs while downloading a file."""
 
     pass
+
 
 class MergeError(StreamBaseError):
     """Exception raised when an error occurs while merging files."""
