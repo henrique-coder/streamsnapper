ff111d34bdaf462f5ec3a0d417eff2199b9351c3
Henrique <henriquemoreira10fk@gmail.com>
2024-11-19 13:52:31 -0300

    Removed SoundCloud scraper (temporarily)

--- streamsnapper/__init__.py
+++ streamsnapper/__init__.py
@@ -1,6 +1,5 @@
 # Local imports
 from .platforms.youtube import YouTube, YouTubeExtractor
-from .platforms.soundcloud import SoundCloud, SoundCloudExtractor
 from .downloader import Downloader
 from .merger import Merger
 from .exceptions import StreamBaseError, EmptyDataError, InvalidDataError, ScrapingError, DownloadError, MergeError
@@ -12,8 +11,6 @@ __license__ = 'MIT'
 __all__ = [
     'YouTube',
     'YouTubeExtractor',
-    'SoundCloud',
-    'SoundCloudExtractor',
     'Downloader',
     'Merger',
     'StreamBaseError',
