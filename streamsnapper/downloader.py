859da48034e19997e421e28f8858d406bbc8c809
Henrique <henriquemoreira10fk@gmail.com>
2024-11-22 17:49:02 -0300

    Refactored entire Downloader class

--- streamsnapper/downloader.py
+++ streamsnapper/downloader.py
@@ -1,11 +1,28 @@
 # Built-in imports
 from pathlib import Path
 from os import PathLike
-from typing import Optional, Union
+from threading import Thread
+from contextlib import nullcontext
+from mimetypes import guess_extension as guess_mimetype_extension
+from queue import Queue
+from typing import Union, Optional, Literal, Any, Dict
 
 # Third-party imports
 try:
-    from pysmartdl2 import SmartDL
+    from requests import get, head
+except (ImportError, ModuleNotFoundError):
+    pass
+
+try:
+    from rich.progress import (
+        Progress,
+        BarColumn,
+        DownloadColumn,
+        TransferSpeedColumn,
+        TimeRemainingColumn,
+        TextColumn,
+        TimeElapsedColumn,
+    )
 except (ImportError, ModuleNotFoundError):
     pass
 
@@ -16,43 +33,171 @@ from .exceptions import DownloadError
 class Downloader:
     """A class for downloading direct download URLs. Created to download YouTube videos and audio streams. However, it can be used to download any direct download URL."""
 
-    def __init__(self, max_connections: int = 4, show_progress_bar: bool = True, timeout: int = 14400) -> None:
+    def __init__(
+        self,
+        max_connections: Union[int, Literal['auto']] = 'auto',
+        overwrite: bool = False,
+        show_progress_bar: bool = True,
+        headers: Dict[Any, Any] = None,
+        timeout: int = 1440,
+    ) -> None:
         """
         Initialize the Downloader class with the required settings for downloading a file.
 
         :param max_connections: The maximum number of connections (threads) to use for downloading the file.
+        :param overwrite: Overwrite the file if it already exists. Otherwise, a "_1", "_2", etc. suffix will be added to the file name.
         :param show_progress_bar: Show or hide the download progress bar.
+        :param headers: A dictionary of custom headers to be sent with the request.
         :param timeout: The timeout in seconds for the download process.
         """
 
-        self._max_connections: int = max_connections
-        self._show_progress_bar: bool = show_progress_bar
-        self._timeout: int = timeout
+        if isinstance(max_connections, int) and max_connections <= 0:
+            raise ValueError('The number of threads must be greater than 0.')
 
-        self.output_file_path: Optional[str] = None
+        self.max_connections = max_connections
+        self._overwrite = overwrite
+        self._show_progress_bar = show_progress_bar
+        self._headers: Optional[Dict[Any, Any]] = headers
+        self._timeout = timeout
+
+        self._queue: Queue = Queue()
+
+    def _generate_unique_filename(self, file_path: Union[str, PathLike]) -> Path:
+        file_path = Path(file_path)
+
+        if self._overwrite:
+            return file_path
+
+        counter = 1
+        unique_filename = file_path
+
+        while unique_filename.exists():
+            unique_filename = Path(f'{file_path.parent}/{file_path.stem}_{counter}{file_path.suffix}')
+            counter += 1
+
+        return unique_filename
+
+    def _get_filename_from_url(self, url: str, headers: Dict[Any, Any]) -> str:
+        if 'Content-Disposition' in headers:
+            content_disposition = headers['Content-Disposition']
+
+            if 'filename=' in content_disposition:
+                return content_disposition.split('filename=')[-1].strip().strip('"')
+
+        url_filename = url.split('/')[-1]
+
+        if '.' in url_filename:
+            return url_filename
+
+        mimetype = headers.get('Content-Type', '')
+        extension = guess_mimetype_extension(mimetype.split(';')[0]) if mimetype else ''
+
+        return f'file{extension}' if extension else 'file'
+
+    def _calculate_threads(self, file_size: int) -> int:
+        if isinstance(self.max_connections, int):
+            return self.max_connections
+
+        if self.max_connections == 'auto':
+            if file_size <= 5 * 1024 * 1024:  # < 5 MB
+                return 4
+            elif file_size <= 50 * 1024 * 1024:  # 5-50 MB
+                return 8
+            elif file_size <= 200 * 1024 * 1024:  # 50-200 MB
+                return 16
+            elif file_size <= 400 * 1024 * 1024:  # 200-400 MB
+                return 24
+            else:
+                return 32
+
+    def _download_chunk(self, url: str, start: int, end: int, temp_file: Union[str, PathLike]) -> None:
+        headers = {} if self._headers is None else self._headers
+        headers['Range'] = f'bytes={start}-{end}'
+
+        r = get(url, headers=headers, stream=True, allow_redirects=True)
+
+        with Path(temp_file).open('r+b') as f:
+            f.seek(start)
+            f.write(r.content)
+
+    def _worker(self, progress_task_id: Optional[int] = None, progress: Optional[Progress] = None) -> None:
+        while not self._queue.empty():
+            task = self._queue.get()
+
+            try:
+                self._download_chunk(*task)
+
+                if progress and progress_task_id is not None:
+                    progress.update(progress_task_id, advance=task[2] - task[1] + 1)
+            finally:
+                self._queue.task_done()
 
     def download(self, url: str, output_file_path: Union[str, PathLike] = Path.cwd()) -> None:
         """
-        Download the file from the provided URL to the output file path.
+        Download a file from a given URL to a specified output path.
 
-        :param url: The download URL to download the file from.
-        :param output_file_path: The path to save the downloaded file to. If the path is a directory, the file name will be generated from the server response. If the path is a file, the file will be saved with the provided name. If not provided, the file will be saved to the current working directory (from pathlib.Path.cwd()).
-        :raises DownloadError: If an error occurs while downloading the file.
+        :param url: The URL of the file to download.
+        :param output_file_path: The path where the downloaded file will be saved. If it's a directory, the filename will be determined from the URL or server response. If it's a file path, it will save with that name. If overwrite=False and a conflict occurs, a unique name will be generated automatically (e.g., "file_1.ext"). If overwrite=True and a conflict occurs, existing files will be replaced without warning. Defaults to the current working directory.
         """
 
         output_file_path = Path(output_file_path).resolve()
 
-        try:
-            downloader = SmartDL(
-                urls=url,
-                dest=output_file_path.as_posix(),
-                threads=self._max_connections,
-                progress_bar=self._show_progress_bar,
-                timeout=self._timeout,
-            )
-            downloader.start(blocking=True)
-        except Exception as e:
-            raise DownloadError(f'Error occurred while downloading URL: "{url}" to "{output_file_path.as_posix()}"') from e
+        r = head(url, headers=self._headers, allow_redirects=True)
 
-        output_destination = downloader.get_dest()
-        self.output_file_path = Path(output_destination).as_posix() if output_file_path else None
+        if r.status_code != 200 or 'Content-Length' not in r.headers:
+            raise ValueError('Could not determine the file size or access the URL.')
+
+        file_size = int(r.headers['Content-Length'])
+
+        filename = self._get_filename_from_url(url, r.headers)
+        mimetype = r.headers.get('Content-Type', 'unknown')
+
+        if output_file_path.is_dir():
+            output_file_path = Path(output_file_path, filename)
+
+        output_file_path.parent.mkdir(parents=True, exist_ok=True)
+        unique_filename = self._generate_unique_filename(output_file_path)
+
+        with open(unique_filename, 'wb') as f:
+            f.write(b'\0' * file_size)
+
+        threads_count = self._calculate_threads(file_size)
+        chunk_size = file_size // threads_count
+
+        for i in range(threads_count):
+            start = i * chunk_size
+            end = file_size - 1 if i == threads_count - 1 else (start + chunk_size - 1)
+            temp_file = unique_filename
+            self._queue.put((url, start, end, temp_file))
+
+        context_manager = (
+            Progress(
+                TextColumn(f'Downloading "{filename}" ({mimetype}) to "{output_file_path.as_posix()}"'),
+                BarColumn(),
+                DownloadColumn(),
+                TransferSpeedColumn(),
+                TimeRemainingColumn(),
+                TimeElapsedColumn(),
+            )
+            if self._show_progress_bar
+            else nullcontext()
+        )
+
+        with context_manager as progress:
+            task_id = None
+
+            if self._show_progress_bar:
+                task_id = progress.add_task('Downloading', total=file_size)
+
+            threads = []
+
+            for _ in range(threads_count):
+                thread = Thread(
+                    target=self._worker,
+                    kwargs={'progress_task_id': task_id, 'progress': progress if self._show_progress_bar else None},
+                )
+                thread.start()
+                threads.append(thread)
+
+            for thread in threads:
+                thread.join()
