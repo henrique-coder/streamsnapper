a5dc6092ebe5d226d5d9e898ff1b133da1f0909b
Henrique <henriquemoreira10fk@gmail.com>
2024-11-25 18:38:39 -0300

    Undo version to 0.2.1

--- streamsnapper/__init__.py
+++ streamsnapper/__init__.py
@@ -5,7 +5,7 @@ from .merger import Merger
 from .exceptions import StreamBaseError, EmptyDataError, InvalidDataError, ScrapingError, DownloadError, MergeError
 
 
-__version__ = '0.2.2'
+__version__ = '0.2.1'
 __license__ = 'MIT'
 
 __all__ = [
