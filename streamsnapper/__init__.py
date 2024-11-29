25162de90aaceb00d73c06749d6fd7069b975cfe
Henrique <henriquemoreira10fk@gmail.com>
2024-11-28 21:29:39 -0300

    Sorted imports

--- streamsnapper/__init__.py
+++ streamsnapper/__init__.py
@@ -3,9 +3,9 @@ from typing import List
 
 # Local imports
 from .downloader import Downloader
+from .exceptions import StreamBaseError, EmptyDataError, InvalidDataError, ScrapingError, DownloadError, MergeError
 from .merger import Merger
 from .platforms.youtube import YouTube, YouTubeExtractor
-from .exceptions import StreamBaseError, EmptyDataError, InvalidDataError, ScrapingError, DownloadError, MergeError
 
 
 __all__: List[str] = [
