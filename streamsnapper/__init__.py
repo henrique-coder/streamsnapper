ef68f9c2f4ff3093407498380738cb5b4a41dd23
Henrique <henriquemoreira10fk@gmail.com>
2024-12-11 20:06:08 -0300

    Sorted imports and variables

--- streamsnapper/__init__.py
+++ streamsnapper/__init__.py
@@ -3,20 +3,21 @@ from typing import List
 
 # Local imports
 from .downloader import Downloader
-from .exceptions import StreamBaseError, EmptyDataError, InvalidDataError, ScrapingError, DownloadError, MergeError
+from .exceptions import DownloadError, EmptyDataError, InvalidDataError, MergeError, RequestError, ScrapingError, StreamBaseError
 from .merger import Merger
 from .platforms.youtube import YouTube, YouTubeExtractor
 
 
 __all__: List[str] = [
     'Downloader',
+    'DownloadError',
+    'EmptyDataError',
+    'InvalidDataError',
+    'MergeError',
+    'RequestError',
+    'ScrapingError',
+    'StreamBaseError',
     'Merger',
     'YouTube',
     'YouTubeExtractor',
-    'StreamBaseError',
-    'EmptyDataError',
-    'InvalidDataError',
-    'ScrapingError',
-    'DownloadError',
-    'MergeError',
 ]
