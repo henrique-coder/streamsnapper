89dee40d86ae2e3b7a4dbb95f4a4ca379ee21181
Henrique <henriquemoreira10fk@gmail.com>
2024-12-12 12:51:33 -0300

    Refactored tests from unittest to pytest and removed rich module

--- tests/all.py
+++ tests/all.py
@@ -2,11 +2,10 @@
 from pathlib import Path
 from random import choice
 from typing import List, Optional, Tuple
-from unittest import TestCase, main
 
 # Third-party imports
 from orjson import loads as orjson_loads
-from rich.console import Console
+from pytest import fail, fixture, mark
 
 # Local imports
 from streamsnapper import (
@@ -24,39 +23,38 @@ from streamsnapper import (
     YouTubeExtractor,
 )
 
-console = Console()
 
-
-class TestDownloader(TestCase):
-    def setUp(self: TestCase) -> None:
-        self.test_download_urls: List[str] = [
+class TestDownloader:
+    @fixture
+    def download_urls(self) -> List[str]:
+        return [
             'https://httpbin.org/image/jpeg',
             'https://httpbin.org/image/png',
             'https://httpbin.org/image/svg',
             'https://httpbin.org/image/webp',
         ]
 
-    def test_file_download(self: TestCase) -> None:
+    def test_file_download(self, download_urls: List[str]) -> None:
         downloader: Downloader = Downloader(
             max_connections='auto', overwrite=True, show_progress_bar=True, headers=None, timeout=10
         )
 
         try:
-            downloader.download(url=choice(self.test_download_urls), output_path=Path.cwd())
+            downloader.download(url=choice(download_urls), output_path=Path.cwd())
         except (DownloadError, RequestError, StreamBaseError) as e:
-            self.fail(f'Something went wrong while downloading a file. Error: {e}')
+            fail(f'Something went wrong while downloading a file. Error: {e}')
 
 
-class TestMerger(TestCase):
-    def test_merger_initialization(self: TestCase) -> None:
+class TestMerger:
+    def test_merger_initialization(self) -> None:
         try:
             Merger()
         except (FFmpegNotFoundError, MergeError, StreamBaseError) as e:
-            self.fail(f'Something went wrong while initializing the Merger class. Error: {e}')
+            fail(f'Something went wrong while initializing the Merger class. Error: {e}')
 
 
-class TestYouTube(TestCase):
-    def test_video_data_extractor(self: TestCase) -> None:
+class TestYouTube:
+    def test_video_data_extractor(self) -> None:
         youtube: YouTube = YouTube(logging=False)
 
         try:
@@ -66,19 +64,20 @@ class TestYouTube(TestCase):
             youtube.analyze_audio_streams(preferred_language='local')
             youtube.analyze_subtitle_streams()
         except (ValueError, InvalidDataError, ScrapingError, InvalidDataError, EmptyDataError, StreamBaseError) as e:
-            self.fail(f'Something went wrong while extracting a YouTube video data. Error: {e}')
+            fail(f'Something went wrong while extracting a YouTube video data. Error: {e}')
 
-        self.assertIsNotNone(youtube.general_info, 'General information is not available.')
-        self.assertIsNotNone(youtube.best_video_streams, 'Video streams are not available.')
-        self.assertIsNotNone(youtube.best_audio_streams, 'Audio streams are not available.')
-        self.assertIsNotNone(youtube.subtitle_streams, 'Subtitle streams are not available.')
+        assert youtube.general_info is not None, 'General information is not available.'
+        assert youtube.best_video_streams is not None, 'Video streams are not available.'
+        assert youtube.best_audio_streams is not None, 'Audio streams are not available.'
+        assert youtube.subtitle_streams is not None, 'Subtitle streams are not available.'
 
 
-class TestYouTubeExtractor(TestCase):
-    def setUp(self: TestCase) -> None:
-        self.youtube_extractor: YouTubeExtractor = YouTubeExtractor()
+class TestYouTubeExtractor:
+    @fixture
+    def youtube_extractor(self) -> YouTubeExtractor:
+        return YouTubeExtractor()
 
-    def test_platform_identifier(self: TestCase) -> None:
+    def test_platform_identifier(self, youtube_extractor: YouTubeExtractor) -> None:
         urls: List[Tuple[str, str]] = [
             ('https://music.youtube.com/watch?v=01234567890&list=RD012345678901234', 'youtubeMusic'),
             ('https://music.youtube.com/watch?v=01234567890', 'youtubeMusic'),
@@ -87,14 +86,12 @@ class TestYouTubeExtractor(TestCase):
         ]
 
         for url, expected_platform in urls:
-            identified_platform = self.youtube_extractor.identify_platform(url)
-            self.assertEqual(
-                identified_platform,
-                expected_platform,
-                f'URL: "{url}" - Identified platform: "{identified_platform}" - Expected platform: "{expected_platform}"',
+            identified_platform = youtube_extractor.identify_platform(url)
+            assert identified_platform == expected_platform, (
+                f'URL: "{url}" - Identified platform: "{identified_platform}" - ' f'Expected platform: "{expected_platform}"'
             )
 
-    def test_video_id_extractor(self: TestCase) -> None:
+    def test_video_id_extractor(self, youtube_extractor: YouTubeExtractor) -> None:
         urls: List[Tuple[str, str]] = [
             ('https://www.youtube.com/watch?v=01234567890', '01234567890'),
             ('https://youtu.be/01234567890', '01234567890'),
@@ -103,14 +100,12 @@ class TestYouTubeExtractor(TestCase):
         ]
 
         for url, expected_video_id in urls:
-            extracted_video_id = self.youtube_extractor.extract_video_id(url)
-            self.assertEqual(
-                extracted_video_id,
-                expected_video_id,
-                f'URL: "{url}" - Extracted video ID: "{extracted_video_id}" - Expected video ID: "{expected_video_id}"',
+            extracted_video_id = youtube_extractor.extract_video_id(url)
+            assert extracted_video_id == expected_video_id, (
+                f'URL: "{url}" - Extracted video ID: "{extracted_video_id}" - ' f'Expected video ID: "{expected_video_id}"'
             )
 
-    def test_playlist_id_extractor(self: TestCase) -> None:
+    def test_playlist_id_extractor(self, youtube_extractor: YouTubeExtractor) -> None:
         urls: List[Tuple[str, Optional[str]]] = [
             ('https://youtu.be/01234567890', None),
             ('https://music.youtube.com/watch?v=01234567890&list=RD012345678901234', None),
@@ -123,22 +118,21 @@ class TestYouTubeExtractor(TestCase):
         ]
 
         for url, expected_playlist_id in urls:
-            extracted_playlist_id = self.youtube_extractor.extract_playlist_id(url, include_private=False)
-            self.assertEqual(
-                extracted_playlist_id,
-                expected_playlist_id,
-                f'Include Private: "False" - URL: "{urls}" - Extracted playlist ID: "{extracted_playlist_id}" - Expected playlist ID: "{expected_playlist_id}"',
+            extracted_playlist_id = youtube_extractor.extract_playlist_id(url, include_private=False)
+            assert extracted_playlist_id == expected_playlist_id, (
+                f'Include Private: "False" - URL: "{urls}" - '
+                f'Extracted playlist ID: "{extracted_playlist_id}" - '
+                f'Expected playlist ID: "{expected_playlist_id}"'
             )
 
-    def test_video_search(self: TestCase) -> None:
-        pass  # CPU/Network heavy test - Skipped
+    @mark.skip(reason='CPU/Network heavy test, please run manually')
+    def test_video_search(self) -> None:
+        pass
 
-    def test_playlist_videos_extractor(self: TestCase) -> None:
-        pass  # CPU/Network heavy test - Skipped
+    @mark.skip(reason='CPU/Network heavy test, please run manually')
+    def test_playlist_videos_extractor(self) -> None:
+        pass
 
-    def test_channel_videos_extractor(self: TestCase) -> None:
-        pass  # CPU/Network heavy test - Skipped
-
-
-if __name__ == '__main__':
-    main()
+    @mark.skip(reason='CPU/Network heavy test, please run manually')
+    def test_channel_videos_extractor(self) -> None:
+        pass
