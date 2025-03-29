ec3731a97b7fee27937b54618812b3f529a98dea
Henrique <henriquemoreira10fk@gmail.com>
2025-03-29 14:39:03 -0300

    refactor: update type hinting in __init__.py to use built-in list type

--- streamsnapper/__init__.py
+++ streamsnapper/__init__.py
@@ -1,13 +1,10 @@
-# Built-in imports
-from typing import List
-
 # Local imports
 from .exceptions import EmptyDataError, FFmpegNotFoundError, InvalidDataError, MergeError, ScrapingError, StreamSnapperError
 from .merger import Merger
 from .scraper import YouTube, YouTubeExtractor
 
 
-__all__: List[str] = [
+__all__: list[str] = [
     "EmptyDataError",
     "FFmpegNotFoundError",
     "InvalidDataError",
