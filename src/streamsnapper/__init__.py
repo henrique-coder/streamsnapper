95c42867902c1312d40172d41726fadcda1c5e92
henrique-coder <henriquemoreira10fk@gmail.com>
2025-06-11 17:56:06 -0300

    refactor: add CookieFile and SupportedCookieBrowser to __all__ for improved module exports

--- src/streamsnapper/__init__.py
+++ src/streamsnapper/__init__.py
@@ -7,16 +7,19 @@ from .exceptions import (
     UnsupportedPlatformError,
     VideoNotFoundError,
 )
+from .utils import CookieFile, SupportedCookieBrowser
 from .core import YouTube, YouTubeExtractor
 
 
 __all__ = [
+    "CookieFile",
     "InvalidDataError",
     "NetworkError",
     "ScrapingError",
     "StreamSnapperError",
     "UnsupportedPlatformError",
     "VideoNotFoundError",
+    "SupportedCookieBrowser",
     "YouTube",
     "YouTubeExtractor",
 ]
